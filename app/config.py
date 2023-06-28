7b9e84c40c4410a88f22dc14b16af9b19ae5acce
KPEGAN <luckpegan@gmail.com>
2023-06-28 23:31:23 +0000

    init

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
 class Config:
     SECRET_KEY = 'qwerty'
     SQL_DATABASE_URI = 'postgresql://postgres:bayernmunich@localhost/robot'
+
+
+
+# git remote add origin https://github.com/nouveau-utilisateur/nouveau-depot.git
